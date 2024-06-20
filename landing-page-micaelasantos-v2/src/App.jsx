cf8ade71aa8466e15f69acd1dee59cb34d2d08c8
Danilo Cassiano <93688833+danilocassiano@users.noreply.github.com>
2024-06-20 11:22:11 -0300

    feat: creating a routine sobre

--- landing-page-micaelasantos-v2/src/App.jsx
+++ landing-page-micaelasantos-v2/src/App.jsx
@@ -2,6 +2,7 @@ import Home from "./components/Home/Home.jsx"
 import Infos from "./components/Infos/Infos.jsx"
 import NavbarHome from "./components/Navbar/Navbar.jsx"
 import Servicos from "./components/Servicos/Servicos.jsx"
+import Sobre from "./components/Sobre/Sobre.jsx"
 
 
 function App() {
@@ -10,7 +11,8 @@ function App() {
     <NavbarHome />    
     <Home /> 
     <Infos /> 
-    <Servicos />      
+    <Servicos />   
+    <Sobre />   
     </>
   )
 }
---
+++ landing-page-micaelasantos-v2/src/components/Sobre/Sobre.jsx
@@ -0,0 +1,35 @@
+import styles from './index.module.scss';
+import foto from './assets/sobre.png';
+import clsx from 'clsx';
+import { Row, Col, Container } from 'react-bootstrap';
+
+const Sobre = () => {
+    return (
+        <section id='sobre' className={clsx(styles.s_sobre)}>
+            <Container>
+                <h2 data-aos="slide-down" data-aos-duration="1000" className={clsx(styles.titulo, 'text-center pt-4 pb-4')}>Micaela Santos <br/>CRP 02/27486</h2>
+                <Row className='my-auto'>
+                    <Col data-aos="fade-right" data-aos-duration="1000" data-aos-delay="350" lg={7}>
+                        <p className={styles.desc}>
+                            Micaela Santos é psicóloga clínica Psicanalítica. Com vasta experiência na área, atuo tanto em atendimento clínico presencial quanto online, oferecendo suporte emocional e terapia para adultos, adolescentes e crianças.
+                            <br />
+                            <br />
+                            Tem uma abordagem sensível e empática, priorizando a escuta atenta e o acolhimento de cada pessoa que busca minha ajuda. Acredito profundamente na importância do processo terapêutico como uma jornada de autoconhecimento e transformação, onde é possível explorar pensamentos, sentimentos e experiências, compreendendo as raízes dos conflitos e buscando soluções que promovam um bem-estar emocional duradouro.
+                            <br />
+                            <br />
+                            Minha especialização em Parentalidade e Psicanálise com Criança me permite oferecer um suporte ainda mais direcionado às necessidades das famílias, ajudando pais e filhos a construírem relacionamentos saudáveis e compreensivos.
+                            <br />
+                            <br />
+                            Se você está enfrentando desafios emocionais, buscando compreender melhor a si mesmo ou lidar com questões específicas, estou aqui para ajudar. Entre em contato e vamos juntos iniciar essa jornada rumo ao seu equilíbrio emocional e desenvolvimento pessoal.
+                        </p>
+                    </Col>
+                    <Col data-aos="fade-out" data-aos-duration="1000" data-aos-delay="350" lg={5}>
+                        <img className={clsx(styles.image)} src={foto} alt="Foto Dra. Thayse" />
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    )
+}
+
+export default Sobre
