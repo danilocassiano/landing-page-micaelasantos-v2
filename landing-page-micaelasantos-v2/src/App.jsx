4437a346dad36e208de8fe36a883ccfb32beeb59
Danilo Cassiano <93688833+danilocassiano@users.noreply.github.com>
2024-06-20 10:37:48 -0300

    fix: add div's infos e serviços

--- landing-page-micaelasantos-v2/src/App.jsx
+++ landing-page-micaelasantos-v2/src/App.jsx
@@ -1,13 +1,16 @@
 import Home from "./components/Home/Home.jsx"
+import Infos from "./components/Infos/Infos.jsx"
 import NavbarHome from "./components/Navbar/Navbar.jsx"
+import Servicos from "./components/Servicos/Servicos.jsx"
 
 
 function App() {
    return (
     <>
     <NavbarHome />    
-    <Home />  
-      
+    <Home /> 
+    <Infos /> 
+    <Servicos />      
     </>
   )
 }
