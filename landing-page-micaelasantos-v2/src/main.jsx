b1f6e0cbd83b815c38cd0a9f5f513757b1d8febe
Danilo Cassiano <93688833+danilocassiano@users.noreply.github.com>
2024-06-21 20:27:47 -0300

    fix: Remoção do Clsx

--- landing-page-micaelasantos-v2/src/main.jsx
+++ landing-page-micaelasantos-v2/src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/global.scss';
-import 'aos/dist/aos.css'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
